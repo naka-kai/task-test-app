de439111fa2744352a953b8877aca5779c5d6c79
kai <sea.k.naka@gmail.com>
2023-08-05 11:08:20 +0900

    postsテーブル作成とテストデータ投入

---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('posts')->insert([
+            [
+                'name' => '太郎',
+                'content' => '筋トレ',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => '花子',
+                'content' => '読書',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ]);
+    }
+}
